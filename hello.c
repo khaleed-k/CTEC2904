0f7bb53c81998bfbc6d34a84e119fa755bd4fc49
Khaleed Kadir <P2693675@LEC-ADMIN.dmu.ac.uk>
2022-11-24 11:04:09 +0000

    added hello.c

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Fare thee well!");
+   printf("Hello World!");
+	printf("Quicker version");
+   return 0;
+}
